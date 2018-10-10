687fc846a8e412a00666e0783f45e8d7a5895dd9
LokerZ <ridwan.abdurrasyid@yahoo.com>
2018-10-11 00:48:18 +0700

    PersonaKitty v1.0

---
+++ Assets/ScreenFix.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFix : MonoBehaviour {
+
+	void Awake(){
+		#if UNITY_STANDALONE
+		Screen.SetResolution(405,648,false);
+		Screen.fullScreen =false ;
+		#endif
+	}
+}
---
+++ Assets/Speech.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speech : MonoBehaviour {
+	Text textbox;
+	// Use this for initialization
+	
+	public void SpeechTrigger(string speech){
+		textbox = GetComponentInChildren<Text> ();
+		textbox.text = speech;
+		StopCoroutine("deactivate");
+		StartCoroutine("deactivate");
+	}
+
+	IEnumerator deactivate(){
+		while (true){
+			yield return new WaitForSeconds (3);
+			gameObject.SetActive (false);
+		}
+	}
+}
